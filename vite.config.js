5efdbf31faf85044881923d153352678465eae51
illia-stack <illiashapshalov38@gmail.com>
2026-05-06 15:05:19 +0200

    g

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-// frontend_react/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     host: true,
     port: 5173,
-    preview: {
-      allowedHosts: ["bookingfront-b9j1.onrender.com"], // <- hier Host eintragen
-    },
+  },
+
+  // <-- preview muss hier AUßERHALB von server stehen
+  preview: {
+    allowedHosts: ["bookingfront-b9j1.onrender.com"],
   },
 });
