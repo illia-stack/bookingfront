1dbf8d403ea8a84037cac3cd3d675458a3a2f4df
illia-stack <illiashapshalov38@gmail.com>
2026-05-08 18:34:06 +0200

    mmm

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 import './index.css'
-import "./App.css";
 import './styles/global.css'
 
 createRoot(document.getElementById('root')).render(
