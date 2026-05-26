bcc2ea845c31c739bb70398eda4c172e7bd93a7e
illia-stack <illiashapshalov38@gmail.com>
2026-05-26 02:04:44 +0200

    prop

--- src/api/propertyApi.js
+++
@@ -1,9 +0,0 @@
-import api from "./client";
-
-export const getProperties = () => {
-  return api.get("/properties");
-};
-
-export const getProperty = (id) => {
-  return api.get(`/properties/${id}`);
-};
