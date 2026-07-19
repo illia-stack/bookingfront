59b350f46ee918a0654704d412ce04d10071e12a
illia-stack <illiashapshalov38@gmail.com>
2026-07-19 21:07:17 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
+console.log("VITE CONFIG LOADED");
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
-    react({
-      include: [
-        "**/*.jsx",
-        "**/*.js"
-      ]
-    })
-  ]
+    react()
+  ],
+
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.js$/,
+  },
 });
