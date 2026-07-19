70396a2668e00e8f3e868c9c0ae88036bdaad3bd
illia-stack <illiashapshalov38@gmail.com>
2026-07-19 20:59:51 +0200

    Fix Vite JSX parsing for index.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
-    react()
-  ],
-
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.js$/,
-  },
+    react({
+      include: [
+        "**/*.jsx",
+        "**/*.js"
+      ]
+    })
+  ]
 });
