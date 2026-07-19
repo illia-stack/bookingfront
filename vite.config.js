23493693afb14039d3c500a84091ed69e501bcea
illia-stack <illiashapshalov38@gmail.com>
2026-07-19 20:54:51 +0200

    fix2

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
 
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
-
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 10000,
-    allowedHosts: [
-      "bookingfront-b9j1.onrender.com",
-    ],
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.js$/,
   },
 });
