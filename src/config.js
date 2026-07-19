4d7bab64fc7c0a358ba506538dc9033cd2493319
illia-stack <illiashapshalov38@gmail.com>
2026-07-19 12:38:50 +0200

    right_config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-export const API_BASE_URL = "https://plantback.onrender.com/api";
-
-export const BACKEND_URL = "https://plantback.onrender.com";
+export const API_BASE_URL = "https://bookingback-working.onrender.com";
+export const BACKEND_URL = "https://bookingback-working.onrender.com";
