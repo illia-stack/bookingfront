1fda312f120d48c7fe90b0ce4f44247c2deb6c02
illia-stack <illiashapshalov38@gmail.com>
2026-07-20 17:36:05 +0200

    contact

--- src/api/contact.js
+++ src/api/contact.js
@@ -1,17 +1,13 @@
 import { API_BASE_URL } from "../config";
 
 export const sendContact = async (data) => {
-
-  const res = await fetch(
-    `${API_BASE_URL}/contact`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }
-  );
+  const res = await fetch(`${API_BASE_URL}/contact.php`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 
   const json = await res.json();
 
